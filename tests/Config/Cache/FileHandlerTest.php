9496bb32600c1272006aa1925251e01b38446ae1
Ersin Güvenç <eguvenc@gmail.com>
2018-03-28 21:39:16 +0300

    added memcached and redis services to travis.yml

--- tests/Config/Cache/FileHandlerTest.php
+++ tests/Config/Cache/FileHandlerTest.php
@@ -6,13 +6,39 @@ class FileHandlerTest extends PHPUnit_Framework_TestCase
 {
     public function setup()
     {
-       $this->cache = new FileHandler('/tests/var/cache/config');
+        $this->filename = ROOT.'/tests/Resources/app.yaml';
+        $this->cache = new FileHandler('/tests/var/cache/config');
     }
 
     public function testHas()
     {
-        $filename = ROOT.'/tests/Resources/app.yaml';
-        $this->cache->write($filename, array('test' => 123456));
-        $this->assertTrue($this->cache->has($filename));
+        $this->cache->write($this->filename, array('test' => 123456));
+        $this->assertTrue($this->cache->has($this->filename));
+    }
+
+    public function testRead()
+    {
+        $this->cache->write($this->filename, array('int' => 6789, 'str' => 'foo'));
+        $data = $this->cache->read($this->filename);
+
+        $this->assertEquals($data['int'], 6789);
+        $this->assertEquals($data['str'], 'foo');
+        $this->assertArrayNotHasKey('__mtime__', $data);
+    }
+
+    public function testWrite()
+    {
+        $this->cache->write(
+            $this->filename,
+            [
+                'app' => [
+                    'session' => [
+                        'name' => 'sessions'
+                    ]
+                ]
+            ]
+        );
+        $data = $this->cache->read($this->filename);
+        $this->assertEquals($data['app']['session']['name'], 'sessions');
     }
 }
