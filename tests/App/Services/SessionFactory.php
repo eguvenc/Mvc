e7f2b6ce1d1715b02d10814cccf5007626af1a4e
Ersin Güvenç <eguvenc@gmail.com>
2018-05-27 20:53:10 +0300

    updated services

---
+++ tests/App/Services/SessionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\App\Services;
+
+use Zend\Session\SessionManager;
+use Zend\Session\Validator\HttpUserAgent;
+use Zend\Session\Storage\SessionArrayStorage;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class SessionFactory implements FactoryInterface
+{
+    /**
+     * Create an object
+     *
+     * @param  ContainerInterface $container
+     * @param  string             $requestedName
+     * @param  null|array         $options
+     * @return object
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $manager = null;
+        $manager = new SessionManager();
+        $manager->setStorage(new SessionArrayStorage());
+        // $manager->setName('test');  // No need to set name in test environment may occurs exception.
+        $manager->start();
+
+        return $manager;
+    }
+}
---
+++ tests/App/Services/ViewPlatesFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\App\Services;
+
+use Obullo\Mvc\View\Helper;
+use Obullo\Mvc\View\PlatesPhp;
+use League\Plates\Engine;
+use League\Plates\Extension\Asset;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class ViewPlatesFactory implements FactoryInterface
+{
+    /**
+     * Create an object
+     *
+     * @param  ContainerInterface $container
+     * @param  string             $requestedName
+     * @param  null|array         $options
+     * @return object
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $engine = new Engine(ROOT.'/tests/App/View');
+        $engine->setFileExtension('phtml');
+        $engine->addFolder('templates', ROOT.'/tests/var/templates');
+        // $engine->loadExtension(new Asset(ROOT.'/public/'.strtolower(APP).'/', false));
+
+        /**
+         * View helpers
+         */
+        $engine->registerFunction('url', new Helper\Url($container));
+        $engine->registerFunction('escapeHtml', new Helper\EscapeHtml);
+        $engine->registerFunction('escapeHtmlAttr', new Helper\EscapeHtmlAttr);
+        $engine->registerFunction('escapeCss', new Helper\EscapeCss);
+        $engine->registerFunction('escapeJs', new Helper\EscapeJs);
+        $engine->registerFunction('escapeUrl', new Helper\EscapeUrl);
+
+        $template = new PlatesPhp($engine);
+        $template->setContainer($container);
+
+        return $template;
+    }
+}
